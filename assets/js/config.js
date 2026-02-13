19c766a7292b119073d04551592dbb2400861e17
technocodech <ikrommaulana27588@gmail.com>
2026-02-13 11:05:49 +0700

    Update BASE_URL and APP_NAME in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@
 // BACKEND URL - GANTI SESUAI IP / DOMAIN ANDA
 // ============================================
 // Local development: 
-const BASE_URL = "https://a865-36-70-40-50.ngrok-free.app";
+const BASE_URL = "https://4649-36-70-40-50.ngrok-free.app";
 // 
 // Jaringan lokal: 
 // const BASE_URL = "http://192.168.1.100:8000";
@@ -15,10 +15,11 @@ const BASE_URL = "https://a865-36-70-40-50.ngrok-free.app";
 
 // EXPOSE KE GLOBAL
 window.BASE_URL = BASE_URL;
-window.APP_NAME = "SMKMBP";
+window.APP_NAME = "Sekolah Modern";
 window.PPDB_YEAR = "2026/2027";
 
 // JANGAN UBAH BARIS DI BAWAH INI
 console.log(`🔥 API URL: ${window.BASE_URL}`);
 
 console.log(`📅 PPDB: ${window.PPDB_YEAR}`);
+
